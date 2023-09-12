92dad2b8314fff27ff8bb8c8c90d9f321fdb331b
kevinatwood <atwoodkevin22@gmail.com>
2023-09-11 19:07:51 -0500

    updated file paths

--- app/server.js
+++ app/server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", (req, res)=>{
   res.sendFile(path.join(__dirname,'./Develop/db/public/index.html'))
